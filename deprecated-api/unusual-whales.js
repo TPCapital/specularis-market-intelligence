bf7addc310d24941d69dca09c7e5b3a151f33605
TPCapital <xinxinhaikuo@outlook.com>
2026-05-18 13:15:40 +0800

    Delete deprecated-api directory

--- deprecated-api/unusual-whales.js
+++
@@ -1,6 +0,0 @@
-import { json } from "./_utils.js";
-
-export default async function handler(_req, res) {
-  // Unusual Whales requires authenticated access for reliable options flow.
-  json(res, 501, { error: "Configure an Unusual Whales adapter for live options flow." }, 10);
-}
